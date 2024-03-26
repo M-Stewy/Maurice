3b4208929b5316474aac18f0c09ae318c9dbdce6
Stewy <stewartm1555@gmail.com>
2024-03-25 23:22:17 -0400

    made about half of my level
    still need to make it so turrets deal damage before I make the second half

--- Assets/Scripts/CamFollowPlayer.cs
+++ Assets/Scripts/CamFollowPlayer.cs
@@ -18,15 +18,19 @@ public class CamFollowPlayer : MonoBehaviour
     public float timeOffSet = 0.06f;
     [Tooltip("distance from player while moving in a direction")]
     public float displacement = 5;
+    [Range (1f, 2f)]
+    [Tooltip("How far down the player can look(is multiplied by the displacment")]
+    public float Ymultipler = 2;
 
     private float OffY;
     private float OffX;
+
 
     private Vector3 velocity = Vector3.zero;
     private void Update()
     {
         if (Input.GetAxisRaw("Vertical") == -1)
-            OffY = Input.GetAxis("Vertical") * displacement + YOffset;
+            OffY = Input.GetAxis("Vertical") * displacement * Ymultipler + YOffset;
         else OffY = YOffset;
 
         if (Input.GetAxis("Horizontal") == -1)
