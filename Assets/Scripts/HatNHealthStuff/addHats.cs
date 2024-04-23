1d5510b86d75be4698c505981fadc7061e0e93ee
jchristieLCCC <158184387+jchristieLCCC@users.noreply.github.com>
2024-04-23 16:16:56 -0400

    Added ItemBob script and fixed hat remove error message

--- Assets/Scripts/HatNHealthStuff/addHats.cs
+++ Assets/Scripts/HatNHealthStuff/addHats.cs
@@ -50,14 +50,14 @@ public class addHats : MonoBehaviour
         if (FindObjectOfType<Player>().GetComponent<Player>().immuneFrames) return; //Stops the player from losing a hat even when they are in immune state
         
         childNum = this.transform.childCount;
-        Destroy(this.transform.GetChild(childNum-1).gameObject);
-        decreasePos.Invoke();
-        childForMove = this.transform.GetChild(childNum - 1).gameObject;
-        distance = childForMove.GetComponent<HatPos>().distance;
-        if (childNum-1 != 0)
+        if (childNum != 0)
         {
-            this.transform.position = this.transform.position - new Vector3(0f, distance, 1f);
+            Destroy(this.transform.GetChild(childNum-1).gameObject);
+            decreasePos.Invoke();
+            childForMove = this.transform.GetChild(childNum - 1).gameObject;
+            distance = childForMove.GetComponent<HatPos>().distance;
+
         }
-        
+        if (childNum-1 != 0) {this.transform.position = this.transform.position - new Vector3(0f, distance, 1f); }
     }
 }
---
+++ Assets/Scripts/VisualStuff/ItemBob.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ItemBob : MonoBehaviour
+{ 
+    public float transformAmount=.015f;
+    public float Time=.05f;
+    public int Iterations=30;
+    public int WaitTimeDivisor = 6;
+    private int counter=0;
+    private bool stop = false;
+    private bool isNeg = false;
+    void FixedUpdate()
+    {
+        if (stop) {return; }
+        if (isNeg == true)
+        {
+            this.transform.position = this.transform.position - new Vector3(0f, -transformAmount, 0f);
+            StartCoroutine(wait(Time*(counter/WaitTimeDivisor)));
+            counter++;
+            if (counter == Iterations) { isNeg = false; counter = 0; StartCoroutine(wait(Time)); }
+        } 
+        else
+        {
+            this.transform.position = this.transform.position - new Vector3(0f, transformAmount, 0f);
+            StartCoroutine(wait(Time*(counter/WaitTimeDivisor)));
+            counter++;
+            if (counter == Iterations) {  isNeg = true; counter = 0; StartCoroutine(wait(Time)); }
+        }
+
+    }
+
+    IEnumerator wait(float num)
+    {
+        //Debug.Log("Reached");
+        stop = true;
+        yield return new WaitForSeconds(num);
+        stop = false;
+    }
+}
+    
