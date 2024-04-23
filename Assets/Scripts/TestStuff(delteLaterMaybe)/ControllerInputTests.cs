232a1b9ead0ef8c7461245f1fb80cbad7f4df2fb
jchristieLCCC <158184387+jchristieLCCC@users.noreply.github.com>
2024-04-23 10:18:05 -0400

    Whatever this is idk

--- Assets/Scripts/TestStuff(delteLaterMaybe)/ControllerInputTests.cs
+++ Assets/Scripts/TestStuff(delteLaterMaybe)/ControllerInputTests.cs
@@ -11,47 +11,47 @@ public class ControllerInputTests : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Joystick1Button0))
         {
-            Debug.Log("Joystick 0"); // is square on PS controller
+            Debug.Log("Joystick 0"); // is square on PS controller, A on Xbox
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button1))
         {
-            Debug.Log("Joystick 1"); // is X on PS controller
+            Debug.Log("Joystick 1"); // is X on PS controller, B on Xbox
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button2))
         {
-            Debug.Log("Joystick 2"); // is circle on PS controller
+            Debug.Log("Joystick 2"); // is circle on PS controller, X on Xbox
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button3))
         {
-            Debug.Log("Joystick 3"); // is triganle on PS controller
+            Debug.Log("Joystick 3"); // is triganle on PS controller, Y on Xbox
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button4))
         {
-            Debug.Log("Joystick 4"); // is left trigger on PS controller
+            Debug.Log("Joystick 4"); // is left trigger on PS controller, left bumper on Xbox
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button5))
         {
-            Debug.Log("Joystick 5"); // is right trigger on PS controller
+            Debug.Log("Joystick 5"); // is right trigger on PS controller, right bumper on Xbox
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button6))
         {
-            Debug.Log("Joystick 6"); // is left bumper on PS controller
+            Debug.Log("Joystick 6"); // is left bumper on PS controller, back button on Xbox
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button7))
         {
-            Debug.Log("Joystick 7"); // is right bumper on PS controller
+            Debug.Log("Joystick 7"); // is right bumper on PS controller, Start button on Xbox
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button8))
         {
-            Debug.Log("Joystick 8"); // is "share" button on PS controller
+            Debug.Log("Joystick 8"); // is "share" button on PS controller, Left Stick button on Xbox
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button9))
         {
-            Debug.Log("Joystick 9"); // is "options" button on PS controller
+            Debug.Log("Joystick 9"); // is "options" button on PS controller, Right Stick Button on Xbox
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button10))
         {
-            Debug.Log("Joystick 10"); // is left stick button on PS controller
+            Debug.Log("Joystick 10"); // is left stick button on PS controller, 
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button11))
         {
